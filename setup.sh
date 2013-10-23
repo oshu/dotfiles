a08a5737c2c4695edf65c69b10bedb5450d6c3dd
Joshua Hoffman <joshua@soundcloud.com>
2013-10-23 14:17:28 -0400

    new simple setup example

---
+++ setup.sh
@@ -0,0 +1,16 @@
+# To Setup dotfiles:
+#
+# Edit the line below to point to when you checked out dotfiles,
+# then add all of the lines in the file to your bash profile. 
+DOTFILES="$HOME/Workspace/dotfiles"
+
+# dotfiles profile
+PROFILE="$DOTFILES/profile"
+if [[ -f $PROFILE ]]
+then
+  source $PROFILE
+fi
+
+export DOTFILES
+
+# kate: syntax bash;
